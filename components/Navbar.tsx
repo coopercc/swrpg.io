bab6876b7c280136247f737de4ee1fff59202432
Cooper Cain <cooper@coopercc.me>
2020-05-24 12:09:46 -0700

    wider login button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
           <StarWars width='50' height='50' />
           <h2 className='font-bold pl-2 text-gray-800'>Role Playing Game</h2>
         </div>
-        <button className='ml-auto border border-blue-800 shadow rounded-full px-4 py-2 text-blue-800 hover:bg-blue-800 hover:border-blue-800 hover:text-white transform hover:-translate-y-px active:bg-blue-900 active:border-blue-900 active:translate-y-px'>
+        <button className='ml-auto border border-blue-800 shadow rounded-full px-6 py-2 text-blue-800 hover:bg-blue-800 hover:border-blue-800 hover:text-white transform hover:-translate-y-px active:bg-blue-900 active:border-blue-900 active:translate-y-px'>
           Log in
         </button>
       </div>
