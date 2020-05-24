ed5afd166d1c0b6e36ff72f376f51f68621da110
Cooper Cain <cooper@coopercc.me>
2020-05-24 14:54:34 -0700

    change ico

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div>
       <Head>
         <title>SWRPG Character Sheet</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/svgs/star-wars.svg' />
       </Head>
 
       <main className='w-full'>
