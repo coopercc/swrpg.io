346b7acb9194afccc4d40a9a91fe9cc8191f77ec
Cooper Cain <cooper@coopercc.me>
2020-05-28 17:15:25 -0700

    use env vars

--- firebaseApp.ts
+++ firebaseApp.ts
@@ -1,17 +1,21 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
-const config = {
-  apiKey: 'AIzaSyAkKfAje0suoIif-UfcbDOpdevLazOzGfc',
-  authDomain: 'swrpg-io.firebaseapp.com',
-  databaseURL: 'https://swrpg-io.firebaseio.com',
-  projectId: 'swrpg-io',
-  storageBucket: 'swrpg-io.appspot.com',
-  messagingSenderId: '702394680681',
-  appId: '1:702394680681:web:2e48ac5319b772fdc229f8',
-  measurementId: 'G-6FPRHWVC1P',
-};
+// console.log(process.env);
 
 if (!firebase.apps.length) {
+  const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+
   firebase.initializeApp(config);
 }
 
